4140ff268ffc7dfd7d597589154611c430b920a4
marufyuldashev <cool.backstreet@gmail.com>
2021-07-05 17:18:14 +0500

    commit

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.http import response
+from django.test import SimpleTestCase
 
 # Create your tests here.
+
+class SimpleTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_gallery_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
