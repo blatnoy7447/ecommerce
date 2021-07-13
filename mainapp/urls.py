0a81df21103d8f7424031805c6313845deed59e1
marufyuldashev <cool.backstreet@gmail.com>
2021-07-13 10:41:31 +0500

    modified

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView, GalleryPageView
+from django.conf.urls.static import static
+from ecommerce import settings
+
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('gallery/', GalleryPageView.as_view(), name='gallery'),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(
+    settings.STATIC_URL,document_root=settings.STATICFILES_DIRS
+)
 
