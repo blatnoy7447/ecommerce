c275f2f3e0a6b6ee084ce3db5bb7bf7f243bf1b0
marufyuldashev <cool.backstreet@gmail.com>
2021-07-14 15:31:57 +0500

    admin temp

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView, GalleryPageView
 from django.conf.urls.static import static
 from ecommerce import settings
+from mainapp import AdminViews
 
 
 urlpatterns = [
@@ -9,6 +10,9 @@ urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('gallery/', GalleryPageView.as_view(), name='gallery'),
+
+    # PAGE FOR ADMIN
+    path('admin_home',AdminViews.admin_home)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(
     settings.STATIC_URL,document_root=settings.STATICFILES_DIRS
 )
