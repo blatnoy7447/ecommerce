36d35168b9e01a225a2ce5a5f8c9fafd63b65c87
marufyuldashev <cool.backstreet@gmail.com>
2021-07-16 17:51:23 +0500

    create category page

--- mainapp/AdminViews.py
+++ mainapp/AdminViews.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
+from mainapp.models import Categories
 
 
 @login_required(login_url="/admin/")
 def admin_home(request):
     return render(request,"admin_templates/home.html")
+
+class CategoriesListView(ListView):
+    model=Categories
+    template_name="admin_templates/category_list.html"
+
+class CategoriesCreate(CreateView):
+    model=Categories
+    fields="__all__"
+    template_name="admin_templates/category_create.html"
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('admin_login_process/',views.adminLoginProcess,name="admin_login_process"),
     path('admin_logout_process/',views.adminLogoutProcess,name="admin_logout_process"),
     # PAGE FOR ADMIN
-    path('admin_home/',AdminViews.admin_home,name='admin_home')
+    path('admin_home/',AdminViews.admin_home,name='admin_home'),
+    path('category_list',AdminViews.CategoriesListView.as_view(),name="category_list"),
+    path('category_create',AdminViews.CategoriesCreate.as_view(),name="category_create")
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(
     settings.STATIC_URL,document_root=settings.STATICFILES_DIRS
 )
